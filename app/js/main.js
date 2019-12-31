8232e822af3636d7c00da369768813ab81a1f523
St4y-23 <ghost19011@gmail.com>
2019-12-31 18:56:07 +0400

    added a lot

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,23 @@
 $(function () {
 
-  
+  // accordion
+  $(".accordion_tab").click(function(){
+    $(".accordion_tab").each(function(){
+      $(this).parent().removeClass("active");
+      $(this).removeClass("active");
+    });
+    $(this).parent().addClass("active");
+    $(this).addClass("active");
+});
+
+// slider
+$('.slider__inner').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<button type="button" class="slick-prev arrow__prev"><img src="img/svg/arrow-left.svg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-next arrow__next"><img src="img/svg/arrow-right.svg" alt=""></button>',
+});
+
+
 
 });
