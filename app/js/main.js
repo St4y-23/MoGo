6ef18cb1ad27e2c0af777251839297af1ef54522
St4y-23 <ghost19011@gmail.com>
2020-01-13 18:05:59 +0400

    finish desktop

--- app/js/main.js
+++ app/js/main.js
@@ -18,6 +18,47 @@ $('.slider__inner').slick({
   nextArrow: '<button type="button" class="slick-next arrow__next"><img src="img/svg/arrow-right.svg" alt=""></button>',
 });
 
+ // Fixed header
+ let header = $("#header");
+ let intro = $("#header");
+ // let introH = intro.height();       <----/* Высота элемента без падингов */
+ let introH = intro.innerHeight();          /* Высота элемента с падингами */
+ let scrollPos = $(window).scrollTop();     /* Позиция скролла от самого верха */
+ checkScroll(scrollPos, introH);
+ let nav = $("#nav")
+ let navToggle = $("#navToggle");
+
+ $(window).on("scroll resize", function () {         /* Выполнение действий при скролле, (load) загрузке изменении размера страницы */
+   introH = intro.innerHeight();
+   scrollPos = $(this).scrollTop();                    /* Обновить переменную на текущую позицию скролла */
+   checkScroll(scrollPos, introH);
+
+ });
+
+ function checkScroll(scrollPos, introH) {
+   if (scrollPos > introH) {                           /* Если высота скролла больше чем высота introH, то добавляется класс "fixed", если высота нет то класс убирается */
+     header.addClass("fixed");
+   } else {
+     header.removeClass("fixed");
+   }
+ }
+
+ // Smooth scroll
+ $("[data-scroll").on("click", function (event) {              /* Event event.preventDefault - отменяет стандратное поведение ссылки при клике  */
+  event.preventDefault();
+
+  let elementID = $(this).data("scroll");                   /* Получить ID блока по которому кликнули */
+  let elementOffset = $(elementID).offset().top;            /* Получить отступ от верха страницы  */
+
+  nav.removeClass("show")
+
+  // console.log(elementOffset);
+
+  $("html, body").animate({                                 /* При клике плавно скролит до элемента по которому кликнули */
+    scrollTop: elementOffset - 70                           /* - 50px  */
+  }, 700);                                                /* Скорость прокрутки где 1000 - 1 секунда */
+});
+
 
 
 });
